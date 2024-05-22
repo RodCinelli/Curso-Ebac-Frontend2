e01c232cb93d2dab33bf77082b1b4388e48a17f0
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-05-22 14:55:24 -0300

    Subindo a tarefa

---
+++ main.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    $('#cep').mask('00000-000');
+
+    $('#btn-buscar-cep').click(function () {
+        const cep = $('#cep').val();
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+        const botao = $(this);
+        $(botao).find('i').addClass('d-none')
+        $(botao).find('span').removeClass('d-none')
+
+        //     $.ajax(endpoint).done(function(resposta) {
+        //         const logradouro = resposta.logradouro;
+        //         const bairro = resposta.bairro;
+        //         const cidade = resposta.localidade;
+        //         const estado = resposta.uf;
+        //         const endereco = `${logradouro}, ${bairro}, ${cidade} - ${estado}`;
+        //         $('#endereco').val(endereco);
+
+        //         setTimeout(function () {
+        //             $(botao).find('i').removeClass('d-none')
+        //             $(botao).find('span').addClass('d-none')
+        //         }, 1000);
+        //     });
+
+        fetch(endpoint)
+        .then(function (resposta) {
+            return resposta.json();
+        })
+        .then(function (json) {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro}, ${cidade} - ${estado}`;
+            $('#endereco').val(endereco);
+        })
+        .catch(function (erro) {
+            alert("Ocorreu um erro ai buscar o endereço, tente novamente mais tarde.");
+        })
+        .finally(function () {
+            setTimeout(function () {
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
+            }, 1000);
+        })
+    })
+
+    $('#formulario-pedido').submit(function (evento) {
+        evento.preventDefault();
+        
+        if ($('#nome').val().lenght == 0) {
+            throw new Error('Digite o nome');
+        }
+    })  
+})
